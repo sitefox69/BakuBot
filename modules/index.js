e1ff8920f7242c966400f69215952dba6c75263f
Jacek <fsw2zz526r@privaterelay.appleid.com>
2026-08-31 10:17:00 -0400

    Add files via upload

---
+++ modules/index.js
@@ -0,0 +1,59 @@
+import * as ttv from "./ttv.js";
+import * as steam from "./steam.js";
+import * as xayo from "./xayo.js";
+import * as plan from "./plan.js";
+
+const modules = [
+  ttv,
+  steam,
+  xayo,
+  plan,
+];
+
+export async function handleModules(
+  request,
+  env
+) {
+  for (const module of modules) {
+    if (
+      typeof module.handle ===
+      "function"
+    ) {
+      const response =
+        await module.handle(
+          request,
+          env
+        );
+
+      if (response) {
+        return response;
+      }
+    }
+  }
+
+  return null;
+}
+
+export async function handleDiscordModules(
+  data,
+  env
+) {
+  for (const module of modules) {
+    if (
+      typeof module.handleDiscord ===
+      "function"
+    ) {
+      const response =
+        await module.handleDiscord(
+          data,
+          env
+        );
+
+      if (response) {
+        return response;
+      }
+    }
+  }
+
+  return null;
+}
