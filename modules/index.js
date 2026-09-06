e93a23c2215c1e474e151a6ece5f4ee394284e9b
Jacek <fsw2zz526r@privaterelay.appleid.com>
2026-09-06 17:45:36 +0200

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -2,56 +2,25 @@ import * as ttv from "./ttv.js";
 import * as steam from "./steam.js";
 import * as xayo from "./xayo.js";
 import * as plan from "./plan.js";
-const modules = [
-  ttv,
-  steam,
-  xayo,
-  plan,
-];
+import * as soundboard from "./soundboard.js";
 
-export async function handleModules(
-  request,
-  env,
-  ctx
-) {
+const modules = [ttv, steam, xayo, plan, soundboard];
+
+export async function handleModules(request, env, ctx) {
   for (const module of modules) {
-    if (
-      typeof module.handle ===
-      "function"
-    ) {
-      const response =
-        await module.handle(
-          request,
-          env,
-          ctx
-        );
-      if (response) {
-        return response;
-      }
+    if (typeof module.handle === "function") {
+      const response = await module.handle(request, env, ctx);
+      if (response) return response;
     }
   }
   return null;
 }
 
-export async function handleDiscordModules(
-  data,
-  env,
-  ctx
-) {
+export async function handleDiscordModules(data, env, ctx) {
   for (const module of modules) {
-    if (
-      typeof module.handleDiscord ===
-      "function"
-    ) {
-      const response =
-        await module.handleDiscord(
-          data,
-          env,
-          ctx
-        );
-      if (response) {
-        return response;
-      }
+    if (typeof module.handleDiscord === "function") {
+      const response = await module.handleDiscord(data, env, ctx);
+      if (response) return response;
     }
   }
   return null;
